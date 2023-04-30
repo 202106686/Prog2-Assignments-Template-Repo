368e09d0325b65b62a482e93503fb94ed6c5f59e
202106686 <129951367+202106686@users.noreply.github.com>
2023-04-30 03:01:29 +0200

    Add files via upload

---
+++ assign_02_problem_02_code.java
@@ -0,0 +1,15 @@
+// Ali Abdelrazek
+// 20216686
+import java.util.Scanner;
+ 
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        for (int i = 1; i <= 12; i++) {
+            int result = n * i;
+            System.out.println(n + " * " + i + " = " + result);
+        }
+        input.close();
+    }
+}
